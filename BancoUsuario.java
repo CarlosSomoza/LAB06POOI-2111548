fcc96ba95b402800e5409b1cbf0f25c7c9c4f466
CarlosSomoza <142445282+CarlosSomoza@users.noreply.github.com>
2023-10-20 19:14:03 -0500

    Add files via upload

---
+++ BancoUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancousuario;
+
+/**
+ *
+ * @author carlo
+ */
+public class BancoUsuario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
