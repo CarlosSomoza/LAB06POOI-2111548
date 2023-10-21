102bef8a8842c93b0817000b4fc1f5612206d5af
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-10-20 19:50:16 -0500

    Add files via upload

---
+++ MovimientoCuenta.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bancousuario;
+import java.util.Date;
+
+/**
+ *
+ * @author Melanie
+ */
+public class MovimientoCuenta {
+    // Atributos
+    private String numOperacion;
+    private String descripcion;
+    private Date fecha;
+    private double importe;
+    private String tipoOperacion;
+
+    // Constructor
+    public MovimientoCuenta(String numOperacion, String descripcion, Date fecha, double importe, String tipoOperacion) {
+        this.numOperacion = numOperacion;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.importe = importe;
+        this.tipoOperacion = tipoOperacion;
+    }
+
+    // Métodos
+    public String getNumOperacion() {
+        return numOperacion;
+    }
+
+    public void setNumOperacion(String numOperacion) {
+        this.numOperacion = numOperacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public String getTipoOperacion() {
+        return tipoOperacion;
+    }
+
+    public void setTipoOperacion(String tipoOperacion) {
+        this.tipoOperacion = tipoOperacion;
+    }
+
+    @Override
+    public String toString() {
+        return "MovimientoCuenta [Número de Operación: " + numOperacion + ", Descripción: " + descripcion + ", Fecha: " + fecha + ", Importe: " + importe + ", Tipo de Operación: " + tipoOperacion + "]";
+    }
+}
